dd6e53e9f9154130e83f353221e7329efdf1ccca
Nazarious <116711001+Nazarious-ucu@users.noreply.github.com>
2022-11-08 21:06:53 +0200

    Update target_en.py
    я помітив, що створював бренчі не від main а від інших бренчів, тому видаляю інші функції

--- target_en.py
+++ target_en.py
@@ -6,14 +6,7 @@ def generate_grid() -> List[List[str]]:
     Generates list of lists of letters - i.e. grid for the game.
     e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
     """
-    grid_result = []
-    letter_list = list(map(chr, range(ord('A'), ord('Z') + 1)))
-    for i in range(3):
-        help_list = []
-        for k in range(3):
-            help_list.append(random.choice(letter_list))
-        grid_result.append(help_list)
-    return grid_result
+    pass
 
 
 def get_words(f: str, letters: List[str]) -> List[str]:
